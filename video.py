9aa4d47c89dbc51d7582aa252d27cb0baaca560e
pmallari <princetmallari@gmail.com>
2019-04-02 01:27:25 -0400

    Added the face and eye detection algorithm

--- video.py
+++ video.py
@@ -1,20 +1,31 @@
 import numpy as np 
 import cv2 as cv 
 
-face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
+# Import haarcascade face detector
+face_cascade = cv.CascadeClassifier('cv_xml/haarcascade_frontalface_default.xml')
+eye_cascade  = cv.CascadeClassifier('cv_xml/haarcascade_eye.xml')
 
-img = cv.imread('sachin.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# Set as webcam
+video_capture = cv.VideoCapture(0)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-cv.imshow('img',img)
-cv.waitKey(0)
+while True:
+        ret, frame = video_capture.read()
+
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        eyes  = eye_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x,y,w,h) in faces:
+                cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+
+        for (x,y,w,h) in eyes:
+                cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+        cv.imshow('img',frame)
+        
+        if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+
+video_capture.release()
 cv.destroyAllWindows()
